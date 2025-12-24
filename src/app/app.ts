b47c0259b55034b28ad9968beb8ff7d0cfc63065
unknown <soulmamoudou0@gmail.com>
2025-12-24 13:42:01 +0000

    Apres ajout du navbar et du footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { Navbar } from './shared/navbar/navbar';
+import { Footer } from './shared/footer/footer';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Navbar, Footer, RouterModule]
 })
 export class App {
   protected readonly title = signal('apiRest');
