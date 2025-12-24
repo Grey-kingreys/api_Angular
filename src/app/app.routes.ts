a381f640cd1079703f9136d709114522b8f7e571
unknown <soulmamoudou0@gmail.com>
2025-12-24 13:51:59 +0000

    Apres ajout des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Pays } from './features/pays/pays';
+import { Pokemon } from './features/pokemon/pokemon';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: Home
+        
+    },
+    {
+        path: 'pays',
+        component: Pays
+    },
+    {
+        path: 'pokemon',
+        component: Pokemon
+    },
+];
+
