1e0e1f217d7c68b1b11b31a228f3e3a5362a60ac
unknown <soulmamoudou0@gmail.com>
2025-12-27 01:06:57 +0000

    Apres modification de environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-// src/environments/environment.ts
+// src/environments/environment.prod.ts
 export const environment = {
     production: true,
     apiUrl: 'https://back-api-angular.onrender.com'  // Ton backend NestJS
